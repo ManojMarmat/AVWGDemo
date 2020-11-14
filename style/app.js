d22a4ce61918b2bb1567c0adaa2084002f7b5b45
ManojMarmat <48046764+ManojMarmat@users.noreply.github.com>
2020-11-14 13:58:09 +0530

    Add files via upload

--- style/app.js
+++ style/app.js
@@ -1,1 +1,5 @@
+gsap.fromTo("body",3,{backgroundColor:"#b71c1c"},{backgroundColor:"#eeeeee"})
+gsap.from(".logo", {duration: 1,ease: "back.out(0.5)", x: innerWidth*-1});
+gsap.fromTo(".headcontent",1, {opacity:0,transform:"translateY(-100px)"},{opacity:1,transform:"translateY(0)"});
 
+gsap.from(".nav-link", {stagger:0.1,duration: 1,ease: "back.out(0.5)", x:innerWidth*-1});
